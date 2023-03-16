ce8f1c9dddb3b5807ed320fc0500ad87d48eed5f
ClimberMel <mel.pryor@shaw.ca>
2023-03-15 20:17:41 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'IB Tools'
+copyright = '2023, ClimberMel'
+author = 'ClimberMel'
 
 release = '0.1'
 version = '0.1.0'
