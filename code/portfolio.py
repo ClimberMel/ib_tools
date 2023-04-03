10c7b363ba659b352f91e8f647d4539fda8d8a8e
ClimberMel <mel.pryor@shaw.ca>
2023-04-03 15:24:32 -0700

    add portfolio.py

---
+++ code/portfolio.py
@@ -0,0 +1,112 @@
+# portfolio_08.py
+#   Test parsing PorfolioItem object
+#   This is working, but it is only selecting STK type positions.  Shold expand to get all positions.
+
+from ib_insync import *
+from ib_insync import objects
+import pandas as pd
+import datetime as dt
+import file
+
+parms = file.read_json("parms/parms.json")
+pconnect = parms['connect']
+acct = parms["account"]
+space = ' '
+
+myHoldings  = []      # create a list of my combined positions
+position    = []      # temporary list to hold element from pos during parsing
+
+header = ['Account', 'Alias', 'secType', 'conId', 'Symbol', 'Exchange', 'Currency', 'localSymbol', 'Position', 'avgCost', 'ContractMonth', 'Strike', 'Right', 'Multiplier', 'MarketPrice', 'MarketValue', 'UnrealizedPNL', 'RealizedPNP']
+
+
+for i, a in enumerate(acct):
+    myacct = a['account']
+    alias = a['alias']
+    
+    ib = IB()
+    ib.connect(pconnect["ip"], pconnect["port"], clientId=pconnect["id"], account=myacct)
+    #ib.connect(pconnect["ip"], 7496, clientId=pconnect["id"])
+
+    myPort = ib.portfolio()
+
+    x = dt.datetime.now()                       # get current datetime
+    now_str = x.strftime('%Y-%m-%d_%H-%M')      # format it to string and wothout mili seconds
+
+    for s in myPort:
+        
+        position.clear
+        if s.contract.secType == 'STK':
+            position = [
+                myacct, 
+                alias,
+                s.contract.secType, 
+                s.contract.conId,
+                s.contract.symbol, 
+                s.contract.primaryExchange,
+                s.contract.currency,
+                s.contract.localSymbol,
+                s.position,
+                s.averageCost,
+                None,
+                None,
+                None,
+                None,
+                s.marketPrice,
+                s.marketValue,
+                s.unrealizedPNL, 
+                s.realizedPNL
+                ]
+            myHoldings.append(position)
+        elif s.contract.secType == 'OPT':
+            position = [
+                myacct, 
+                alias,
+                s.contract.secType, 
+                s.contract.conId,
+                s.contract.symbol, 
+                s.contract.primaryExchange,
+                s.contract.currency,
+                s.contract.localSymbol,
+                s.position,
+                s.averageCost,
+                s.contract.lastTradeDateOrContractMonth, 
+                s.contract.strike, 
+                s.contract.right,
+                s.contract.multiplier,
+                s.marketPrice,
+                s.marketValue,
+                s.unrealizedPNL, 
+                s.realizedPNL
+                ]
+            myHoldings.append(position)
+        elif s.contract.secType == 'CASH':
+            position = [
+                myacct, 
+                alias,
+                s.contract.secType, 
+                s.contract.conId,
+                s.contract.symbol, 
+                None,
+                None,
+                s.contract.localSymbol,
+                s.position,
+                s.averageCost,
+                None,
+                None, 
+                None,
+                None,
+                s.marketPrice,
+                s.marketValue,
+                s.unrealizedPNL, 
+                s.realizedPNL
+                ]
+            myHoldings.append(position)
+        else: print('ERROR: '.join(position))
+
+    ib.disconnect()
+
+holdings = pd.DataFrame(myHoldings, columns = header)
+
+# Write DataFrame to CSV File with Default params.
+output_folder = 'output/'
+holdings.to_csv(output_folder + now_str + "_portfolio.csv", index=False)
